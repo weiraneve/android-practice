7b304b97a484d9085e3bc309f998896d7fa40197
weiran1999 <907221539@qq.com>
2022-09-08 20:55:38 +0800

    feat: add the data activity transfer

--- app/src/main/java/com/thoughtworks/android/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/android/MainActivity.kt
@@ -4,19 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.thoughtworks.android.model.Data
 
 class MainActivity : AppCompatActivity() {
 
-    private val mainActivity = "MainActivity"
-    private val create = "onCreate"
-    private val start = "onStart"
-    private val resume = "onResume"
-    private val pause = "onPause"
-    private val stop = "onStop"
-    private val destroy = "onDestroy"
-    private val space = "========================================"
-
+    companion object {
+        private const val REQUEST_CODE = 201
+        private const val mainActivity = "MainActivity"
+        private const val create = "onCreate"
+        private const val space = "========================================"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,29 +27,23 @@ class MainActivity : AppCompatActivity() {
         openContactActivity()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i(mainActivity, start + space)
+    private fun openContactActivity() {
+        val button = findViewById<Button>(R.id.contact)
+        button.setOnClickListener {
+            val intent =  Intent(this, ContactActivity::class.java).apply {
+                val data = Data("jack", "139000000000")
+                putExtra(ContactActivity.CONTACT, data)
+            }
+            startActivityForResult(intent, REQUEST_CODE)
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.i(mainActivity, resume + space)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i(mainActivity, pause + space)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i(mainActivity, stop + space)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(mainActivity, destroy + space)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode  == ContactActivity.RESULT_CODE && data!= null) {
+            val info = data.getParcelableExtra<Data>(ContactActivity.SEND_MAIN)
+            Toast.makeText(this, "${info?.name} : ${info?.phone}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun openLoginActivity() {
@@ -65,14 +58,6 @@ class MainActivity : AppCompatActivity() {
         val constraintButton = findViewById<Button>(R.id.constraint)
         constraintButton!!.setOnClickListener {
             val intent =  Intent(this, ConstraintActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun openContactActivity() {
-        val contactButton = findViewById<Button>(R.id.contact)
-        contactButton!!.setOnClickListener {
-            val intent =  Intent("com.thoughtworks.android.action.0")
             startActivity(intent)
         }
     }
