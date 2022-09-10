9e6d5bd29de33088dac173f2cc3ec9ee2aaa3b77
weiran1999 <907221539@qq.com>
2022-09-10 15:27:04 +0800

    refactor: add the dynamic code to add button

--- app/src/main/java/com/thoughtworks/android/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/android/MainActivity.kt
@@ -1,23 +1,54 @@
 package com.thoughtworks.android
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val buttonContainer: LinearLayout by lazy { findViewById(R.id.button_container) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button0)
-        button!!.setOnClickListener { openNewActivity() }
+
+        generateButtons()
+    }
+
+    private fun generateButtons() {
+        addButton(getString(R.string.button1))
+        addButton(getString(R.string.button2))
+        addButton(getString(R.string.button3))
+        addButton(getString(R.string.button4))
+        addButton(getString(R.string.button5))
+        addButton(getString(R.string.button6))
+        addButton(getString(R.string.button7))
+        addButton(getString(R.string.button8))
+        addButton(getString(R.string.button9))
+        addButton(getString(R.string.button10))
 
     }
 
-    private fun openNewActivity() {
-        val intent =  Intent(this, ConstraintActivity::class.java)
-        startActivity(intent)
+    private fun addButton(name: String, onClickListener: View.OnClickListener? = null) {
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(0, resources.getDimensionPixelSize(R.dimen.margin_24), 0, 0)
+
+        val button = Button(this)
+        button.layoutParams = layoutParams
+        button.text = name
+        button.isAllCaps = false
+
+        onClickListener?.let {
+            button.setOnClickListener(it)
+        }
+
+        buttonContainer.addView(button)
     }
+
 
 }
