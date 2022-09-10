cdaba9199680f654ec22cea6a3bdc4db2580862f
weiran1999 <907221539@qq.com>
2022-09-10 16:14:03 +0800

    refactor: refactor the branch from pre

--- app/src/main/java/com/thoughtworks/android/LoginActivity.kt
+++ app/src/main/java/com/thoughtworks/android/LoginActivity.kt
@@ -6,14 +6,27 @@ import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
-
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
+        initUI()
+    }
 
+    private fun initUI() {
+        hideStatusBar()
+        hideActionBar()
+    }
+
+    private fun hideStatusBar() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         val windowInsetsCompat = WindowInsetsControllerCompat(window, window.decorView)
         windowInsetsCompat.hide(WindowInsetsCompat.Type.statusBars())
+    }
+
+
+    private fun hideActionBar() {
+        val supportActionBar = supportActionBar
+        supportActionBar?.hide()
     }
 }
