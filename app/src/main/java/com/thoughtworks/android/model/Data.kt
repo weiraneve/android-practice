61583af35c28ddf76c9c4d69def9bf3bb01e0833
weiran1999 <907221539@qq.com>
2022-09-11 15:35:59 +0800

    refactor: refactor the pick-contact ability

--- app/src/main/java/com/thoughtworks/android/model/Data.kt
+++
@@ -1,9 +0,0 @@
-package com.thoughtworks.android.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Data(val name: String?, val phone: String?) : Parcelable {
-
-}
