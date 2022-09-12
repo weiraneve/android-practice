63f8826d7496ec72dab0a88f767f1efe4f6c3e6d
weiran1999 <907221539@qq.com>
2022-09-12 16:36:08 +0800

    feat: init viewtype branch

--- app/src/main/java/com/thoughtworks/android/ui/recyclerview/TweetAdapter.kt
+++ app/src/main/java/com/thoughtworks/android/ui/recyclerview/TweetAdapter.kt
@@ -10,9 +10,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.thoughtworks.android.R
 import com.thoughtworks.android.model.Tweet
 
-class TweetAdapter(private val context: Context) : RecyclerView.Adapter<TweetAdapter.TweetViewHolder>() {
+class TweetAdapter(private val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val tweets = ArrayList<Tweet>()
+
+    companion object {
+        private const val VIEW_TYPE_TWEET = 0
+        private const val VIEW_TYPE_BOTTOM = 1
+    }
 
     @SuppressLint("NotifyDataSetChanged")
     fun setData(tweets : List<Tweet>?) {
@@ -21,31 +26,50 @@ class TweetAdapter(private val context: Context) : RecyclerView.Adapter<TweetAda
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetViewHolder {
-        val from = LayoutInflater.from(context)
-        return TweetViewHolder(from.inflate(R.layout.tweet_item_layout, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+        return when (viewType) {
+            VIEW_TYPE_BOTTOM -> BottomViewHolder(
+                inflater.inflate(
+                    R.layout.tweet_bottom,
+                    parent,
+                    false
+                )
+            )
+            else -> TweetViewHolder(inflater.inflate(R.layout.tweet_item_layout, parent, false))
+        }
     }
 
-    override fun onBindViewHolder(holder: TweetViewHolder, position: Int) {
-        val tweet: Tweet = tweets[position]
-        holder.name.text = ""
-        holder.content.text = ""
-        if (tweet.sender == null) {
-            return
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == VIEW_TYPE_TWEET) {
+            val tweetViewHolder = holder as TweetViewHolder
+            val tweet: Tweet = tweets[position]
+            tweetViewHolder.name.text = ""
+            tweetViewHolder.content.text = ""
+            if (tweet.sender == null) {
+                return
+            }
+            tweet.sender?.let {
+                tweetViewHolder.name.text = it.nick
+            }
+            tweetViewHolder.content.text = tweet.content
         }
-        tweet.sender?.let {
-            holder.name.text = it.nick
-        }
-        holder.content.text = tweet.content
     }
+
 
     override fun getItemCount(): Int {
-        return tweets.size
+        return tweets.size + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position < tweets.size) VIEW_TYPE_TWEET else VIEW_TYPE_BOTTOM
     }
 
     class TweetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView = itemView.findViewById(R.id.recy_name)
         val content: TextView = itemView.findViewById(R.id.recy_content)
     }
+
+    class BottomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 }
