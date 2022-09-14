075683d52dcc010c9d7f1214d24812e20c2d2942
weiran1999 <907221539@qq.com>
2022-09-14 21:50:22 +0800

    feat: use snackBar to replace toast

--- app/src/main/java/com/thoughtworks/android/utils/UiUtils.kt
+++ app/src/main/java/com/thoughtworks/android/utils/UiUtils.kt
@@ -1,16 +1,18 @@
 package com.thoughtworks.android.utils
 
 import android.app.Activity
+import android.graphics.Color
 import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.snackbar.Snackbar
 import com.thoughtworks.android.R
 
 class UiUtils {
@@ -34,24 +36,53 @@ class UiUtils {
             transaction.commit()
         }
 
-        fun customToast(activity: Activity, text: String, @DrawableRes drawable: Int, duration: Int): Toast {
-            val inflater: LayoutInflater = activity.layoutInflater
-            val layout: View = inflater.inflate(
-                R.layout.toast,
-                activity.findViewById(R.id.toast_layout_root) as ViewGroup?
-            )
+        fun showSnackBar(
+            activity: Activity,
+            text: String,
+            @DrawableRes drawable: Int,
+            duration: Int
+        ): Snackbar {
+            val layoutHandler: View = activity.findViewById(R.id.handler)
+            val snackBar = Snackbar.make(layoutHandler, text, duration)
+            val viewGroup = snackBar.view as ViewGroup
+            val linearLayout = LinearLayout(layoutHandler.context)
 
-            val image: ImageView = layout.findViewById(R.id.image) as ImageView
-            image.setImageResource(drawable)
-            (layout.findViewById(R.id.text) as TextView).text = text
+            // 加载图片
+            val imageView = ImageView(layoutHandler.context)
+            imageView.setImageResource(drawable)
+            linearLayout.addView(imageView)
+            imageView.layoutParams = getLayoutParamsWithLinearLayout()
+            // 加载文字
+            val textView = TextView(layoutHandler.context)
+            textView.text = text
+            textView.setTextColor(Color.WHITE)
+            textView.layoutParams = getLayoutParamsWithLinearLayout()
 
-            val toast = Toast(activity.applicationContext)
-            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
-            toast.duration = duration
-            toast.view = layout
+            linearLayout.addView(textView)
+            viewGroup.addView(linearLayout)
 
-            return toast
+            snackBar.view.layoutParams = getLayoutParamsWithFrameLayout()
+
+            return snackBar
         }
 
+        private fun getLayoutParamsWithFrameLayout(): FrameLayout.LayoutParams {
+            val layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.gravity = Gravity.CENTER
+            return layoutParams
+        }
+
+        private fun getLayoutParamsWithLinearLayout(): LinearLayout.LayoutParams {
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.gravity = Gravity.CENTER
+            return layoutParams
+        }
     }
+
 }
