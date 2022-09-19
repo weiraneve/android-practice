d0e15a9b7533de31210a63e76082e1e2e96a3836
weiran1999 <907221539@qq.com>
2022-09-19 12:43:42 +0800

    feat: add hideBar

--- app/src/main/java/com/thoughtworks/android/ui/ConstraintActivity.kt
+++ app/src/main/java/com/thoughtworks/android/ui/ConstraintActivity.kt
@@ -1,12 +1,34 @@
-package com.thoughtworks.android.ui
+package com.thoughtworks.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.thoughtworks.android.R
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class ConstraintActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constraint_layout)
+        initUI()
     }
+
+    private fun initUI() {
+        hideStatusBar()
+        hideActionBar()
+    }
+
+    private fun hideStatusBar() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsCompat = WindowInsetsControllerCompat(window, window.decorView)
+        windowInsetsCompat.hide(WindowInsetsCompat.Type.statusBars())
+    }
+
+
+    private fun hideActionBar() {
+        val supportActionBar = supportActionBar
+        supportActionBar?.hide()
+    }
+
 }
