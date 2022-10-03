168b6821cd746fbf9ad944f6cd70ec8a4406444f
weiran1999 <907221539@qq.com>
2022-10-03 16:30:43 +0800

    refactor: extract compose component with top level

--- app/src/main/java/com/thoughtworks/android/ui/compose/ComposeActivity.kt
+++ app/src/main/java/com/thoughtworks/android/ui/compose/ComposeActivity.kt
@@ -3,26 +3,9 @@ package com.thoughtworks.android.ui.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
 import com.thoughtworks.android.common.Constants
-import com.thoughtworks.android.data.model.Sender
 import com.thoughtworks.android.data.model.Tweet
 
 
@@ -44,53 +27,7 @@ class ComposeActivity : AppCompatActivity() {
 
     @Composable
     private fun Content() {
-        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-            tweets.forEach {
-                TweetItem(tweet = it)
-            }
-        }
-    }
-
-    @Composable
-    private fun TweetItem(tweet: Tweet) {
-        Row(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
-            Avatar(tweet.sender)
-            Spacer(modifier = Modifier.width(8.dp))
-            Column {
-                Text(
-                    text = tweet.sender?.nick.orEmpty(),
-                    color = MaterialTheme.colors.primary,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 16.sp
-                )
-                tweet.content.orEmpty().takeIf { it.isNotBlank() }?.let {
-                    Text(
-                        modifier = Modifier
-                            .background(Color.LightGray.copy(alpha = 0.3f))
-                            .fillMaxWidth()
-                            .padding(top = 4.dp, bottom = 4.dp, start = 0.dp, end = 4.dp),
-                        text = it,
-                        color = MaterialTheme.colors.secondaryVariant,
-                        fontSize = 14.sp
-                    )
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun Avatar(sender: Sender?) {
-        sender?.avatar?.let {
-            val painter = rememberAsyncImagePainter(it)
-            Image(
-                painter = painter,
-                contentDescription = "null",
-                Modifier
-                    .size(40.dp)
-                    .clip(CircleShape),
-                contentScale = ContentScale.Crop
-            )
-        }
+        TweetScreen(tweets)
     }
 
     @Preview(showBackground = true)
---
+++ app/src/main/java/com/thoughtworks/android/ui/compose/TweetScreen.kt
@@ -0,0 +1,72 @@
+package com.thoughtworks.android.ui.compose
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.thoughtworks.android.data.model.Sender
+import com.thoughtworks.android.data.model.Tweet
+
+@Composable
+fun TweetScreen(tweets: List<Tweet>) {
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        tweets.forEach {
+            TweetItem(tweet = it)
+        }
+    }
+}
+
+@Composable
+private fun TweetItem(tweet: Tweet) {
+    Row(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+        Avatar(tweet.sender)
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = tweet.sender?.nick.orEmpty(),
+                color = MaterialTheme.colors.primary,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+            tweet.content.orEmpty().takeIf { it.isNotBlank() }?.let {
+                Text(
+                    modifier = Modifier
+                        .background(Color.LightGray.copy(alpha = 0.3f))
+                        .fillMaxWidth()
+                        .padding(top = 4.dp, bottom = 4.dp, start = 0.dp, end = 4.dp),
+                    text = it,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    fontSize = 14.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Avatar(sender: Sender?) {
+    sender?.avatar?.let {
+        val painter = rememberAsyncImagePainter(it)
+        Image(
+            painter = painter,
+            contentDescription = "null",
+            Modifier
+                .size(40.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
