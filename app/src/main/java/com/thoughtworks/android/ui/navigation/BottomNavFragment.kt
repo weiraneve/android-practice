11dd39a88773958380d5570dfdf913290c57e054
weiran1999 <907221539@qq.com>
2023-02-12 16:00:57 +0800

    feat: add settings page

--- app/src/main/java/com/thoughtworks/android/ui/navigation/BottomNavFragment.kt
+++ app/src/main/java/com/thoughtworks/android/ui/navigation/BottomNavFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.thoughtworks.android.R
 
 class BottomNavFragment : Fragment() {
@@ -14,6 +16,28 @@ class BottomNavFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_bottom_nav, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBottomNavBar(view)
+    }
+
+    private fun setupBottomNavBar(view: View) {
+        val toolbar = view.findViewById<Toolbar>(R.id.bottom_nav_toolbar)
+        addToolbarListener(toolbar)
+    }
+
+    private fun addToolbarListener(toolbar: Toolbar) {
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.settings -> {
+                    findNavController().navigate(R.id.settings)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
 }
