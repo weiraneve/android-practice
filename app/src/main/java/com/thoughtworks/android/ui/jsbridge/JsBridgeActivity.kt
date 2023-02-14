eefe0ff45a4f7dddba3e74e4bfe34388a54ec0d5
weiran1999 <907221539@qq.com>
2023-02-14 15:27:40 +0800

    refactor: extract constant and clean code

--- app/src/main/java/com/thoughtworks/android/ui/jsbridge/JsBridgeActivity.kt
+++ app/src/main/java/com/thoughtworks/android/ui/jsbridge/JsBridgeActivity.kt
@@ -14,6 +14,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.lzyzsd.jsbridge.BridgeWebView
+import com.github.lzyzsd.jsbridge.BridgeWebViewClient
 import com.thoughtworks.android.R
 import kotlin.system.exitProcess
 
@@ -23,17 +24,30 @@ import kotlin.system.exitProcess
  */
 class JsBridgeActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var mWebView: BridgeWebView? = null
+    private var bridgeWebView: BridgeWebView? = null
 
     companion object {
-        // URL 网络请求地址 (用局域网IP加对应项目的端口)
-        const val URL = "http://192.168.31.202:8000"
+        // URL 网络请求地址 (用局域网IP加对应项目的端口) eg: http://XXXXX:8000
+        private const val ADDRESS = ""
+        const val URL = "http://$ADDRESS:8000"
+
+        private const val APP = "app"
+        private const val RELOAD_URL = "reloadUrl"
+        private const val ANDROID_FOR_JS_CONTENT = "安卓返回给 JS 的消息内容"
+        private const val REFRESH_SUCCESSFUL = "刷新成功"
+        private const val CHANGE_USER = "changeUser"
+        private const val CHANGE_NAME = "changeName"
+        private const val UPDATE_NAME_SUCCESSFUL = "name 修改成功"
+        private const val COOKIE_SYNC_SUCCESSFUL = "Cookie 同步成功"
+        private const val SYNC_COOKIE = "syncCookie"
+        private const val AGAIN_BACK = "再按一次退出程序"
+        private const val TAG = "JsBridgeActivity"
     }
 
     private var exitTime: Long = 0
-    private var mTvUser: TextView? = null
-    private var mEditName: EditText? = null
-    private var mEditCookie: EditText? = null
+    private var textView: TextView? = null
+    private var editName: EditText? = null
+    private var editCookie: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,53 +59,58 @@ class JsBridgeActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onResume() {
         super.onResume()
-        mWebView!!.reload()
+        bridgeWebView!!.reload()
     }
 
     /**
      * 初始化 WebView
      */
+    @Suppress("DEPRECATION")
     @SuppressLint("SetJavaScriptEnabled")
     private fun initWebView() {
-        mWebView = findViewById(R.id.main_wv)
-
-        mWebView!!.settings.allowFileAccess = true
-        mWebView!!.settings.setAppCacheEnabled(true)
-        mWebView!!.settings.databaseEnabled = true
-        // 开启 localStorage
-        mWebView!!.settings.domStorageEnabled = true
-        // 设置支持javascript
-        mWebView!!.settings.javaScriptEnabled = true
-        // 进行缩放
-        mWebView!!.settings.builtInZoomControls = true
-        // 设置UserAgent
-        mWebView!!.settings.userAgentString = mWebView!!.settings.userAgentString + "app"
-        // 设置不用系统浏览器打开,直接显示在当前WebView
-        mWebView!!.webChromeClient = WebChromeClient()
-        mWebView!!.webViewClient = MyWebViewClient(mWebView!!)
-
-        mWebView!!.loadUrl(URL)
+        bridgeWebView = findViewById(R.id.main_wv)
+        bridgeWebView!!.run {
+            settings.run {
+                allowFileAccess = true
+                setAppCacheEnabled(true)
+                databaseEnabled = true
+                // 开启 localStorage
+                domStorageEnabled = true
+                // 设置支持javascript
+                javaScriptEnabled = true
+                // 进行缩放
+                builtInZoomControls = true
+                // 设置UserAgent
+                userAgentString = bridgeWebView!!.settings.userAgentString + APP
+            }
+            // 设置不用系统浏览器打开,直接显示在当前WebView
+            webChromeClient = WebChromeClient()
+            webViewClient = MyWebViewClient(bridgeWebView!!)
+            loadUrl(URL)
+        }
     }
 
     /**
      * 注册与 H5 交互的事件函数
      */
     private fun registerHandlers() {
-        // 设置默认接收函数
-        mWebView!!.setDefaultHandler { data, function ->
-            Toast.makeText(this@JsBridgeActivity, data, Toast.LENGTH_LONG).show()
-            function.onCallBack("安卓返回给 JS 的消息内容")
-        }
-        // 注册刷新页面的 reloadUrl 函数
-        mWebView!!.registerHandler("reloadUrl") { _, function ->
-            mWebView!!.reload()
-            Toast.makeText(this@JsBridgeActivity, "刷新成功~", Toast.LENGTH_SHORT).show()
-            function.onCallBack("")
-        }
-        // 注册修改 User 名称的 changeUser 函数
-        mWebView!!.registerHandler("changeUser") { user, function ->
-            mTvUser!!.text = user
-            function.onCallBack("")
+        bridgeWebView!!.run {
+            // 设置默认接收函数
+            setDefaultHandler { data, function ->
+                Toast.makeText(this@JsBridgeActivity, data, Toast.LENGTH_LONG).show()
+                function.onCallBack(ANDROID_FOR_JS_CONTENT)
+            }
+            // 注册刷新页面的 reloadUrl 函数
+            registerHandler(RELOAD_URL) { _, function ->
+                reload()
+                Toast.makeText(this@JsBridgeActivity, REFRESH_SUCCESSFUL, Toast.LENGTH_SHORT).show()
+                function.onCallBack("")
+            }
+            // 注册修改 User 名称的 changeUser 函数
+            registerHandler(CHANGE_USER) { user, function ->
+                textView!!.text = user
+                function.onCallBack("")
+            }
         }
     }
 
@@ -99,46 +118,48 @@ class JsBridgeActivity : AppCompatActivity(), View.OnClickListener {
      * 初始化其他 View 组件
      */
     private fun initViews() {
-        findViewById<View>(R.id.btn_cookie).setOnClickListener(this)
-        findViewById<View>(R.id.btn_name).setOnClickListener(this)
-        findViewById<View>(R.id.btn_init).setOnClickListener(this)
-        mTvUser = findViewById(R.id.tv_user)
-        mEditCookie = findViewById(R.id.edit_cookie)
-        mEditName = findViewById(R.id.edit_name)
+        listOf(
+            R.id.btn_cookie,
+            R.id.btn_name,
+            R.id.btn_init
+        ).forEach { findViewById<View>(it).setOnClickListener(this) }
+        textView = findViewById(R.id.tv_user)
+        editCookie = findViewById(R.id.edit_cookie)
+        editName = findViewById(R.id.edit_name)
     }
 
     override fun onClick(view: View) {
         when (view.id) {
+            // 调用 H5 界面的默认接收函数
             R.id.btn_init ->
-                // 调用 H5 界面的默认接收函数
-                mWebView!!.send(
-                    "安卓传递给 JS 的消息"
+                bridgeWebView!!.send(
+                    ANDROID_FOR_JS_CONTENT
                 ) { data -> Toast.makeText(this@JsBridgeActivity, data, Toast.LENGTH_LONG).show() }
 
+            // 调用 H5 界面的 changeName 事件函数
             R.id.btn_name ->
-                // 调用 H5 界面的 changeName 事件函数
-                mWebView!!.callHandler("changeName", mEditName!!.text.toString()) {
-                    Toast.makeText(this@JsBridgeActivity, "name 修改成功", Toast.LENGTH_SHORT)
+                bridgeWebView!!.callHandler(CHANGE_NAME, editName!!.text.toString()) {
+                    Toast.makeText(this@JsBridgeActivity, UPDATE_NAME_SUCCESSFUL, Toast.LENGTH_SHORT)
                         .show()
-                    mEditName!!.setText("")
+                    editName!!.setText("")
                 }
 
+            // 调用 H5 界面的 syncCookie 事件函数
             R.id.btn_cookie -> {
-                syncCookie(this, "token=" + mEditCookie!!.text.toString())
-                // 调用 H5 界面的 syncCookie 事件函数
-                mWebView!!.callHandler("syncCookie", "") {
-                    Toast.makeText(this@JsBridgeActivity, "Cookie 同步成功", Toast.LENGTH_SHORT)
+                syncCookie(this, "token=" + editCookie!!.text.toString())
+                bridgeWebView!!.callHandler(SYNC_COOKIE, "") {
+                    Toast.makeText(this@JsBridgeActivity, COOKIE_SYNC_SUCCESSFUL, Toast.LENGTH_SHORT)
                         .show()
-                    mEditCookie!!.setText("")
+                    editCookie!!.setText("")
                 }
             }
         }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView!!.canGoBack()) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && bridgeWebView!!.canGoBack()) {
             // 返回前一个页面
-            mWebView!!.goBack()
+            bridgeWebView!!.goBack()
             return true
         } else if (keyCode == KeyEvent.KEYCODE_BACK) {
             exit()
@@ -153,7 +174,7 @@ class JsBridgeActivity : AppCompatActivity(), View.OnClickListener {
     private fun exit() {
         if (System.currentTimeMillis() - exitTime > 2000) {
             Toast.makeText(
-                applicationContext, "再按一次退出程序",
+                applicationContext, AGAIN_BACK,
                 Toast.LENGTH_SHORT
             ).show()
             exitTime = System.currentTimeMillis()
@@ -166,18 +187,22 @@ class JsBridgeActivity : AppCompatActivity(), View.OnClickListener {
     /**
      * 这只并同步 Cookie 的工具函数
      * @param context   上下文对象
-     * @param cookie    需要设置的 cookie 值，例如："token=azhd57hkslz"
+     * @param cookie    需要设置的 cookie 值，例如："token=acked57chkstk"
      */
     @Suppress("DEPRECATION")
     private fun syncCookie(context: Context, cookie: String) {
         CookieSyncManager.createInstance(context)
         val cookieManager = CookieManager.getInstance()
-        cookieManager.setAcceptCookie(true)
-        cookieManager.removeSessionCookie()// 移除
-        cookieManager.removeAllCookie()
-        cookieManager.setCookie(URL, cookie)
+        cookieManager.run {
+            setAcceptCookie(true)
+            removeSessionCookie() // 移除
+            removeAllCookie()
+            setCookie(URL, cookie)
+        }
         val newCookie = cookieManager.getCookie(URL)
-        Log.i("tag ", "newCookie == $newCookie")
+        Log.i(TAG, "newCookie == $newCookie")
         CookieSyncManager.getInstance().sync()
     }
 }
+
+class MyWebViewClient(webView: BridgeWebView) : BridgeWebViewClient(webView)
--- app/src/main/java/com/thoughtworks/android/ui/jsbridge/MyWebViewClient.kt
+++
@@ -1,6 +0,0 @@
-package com.thoughtworks.android.ui.jsbridge
-
-import com.github.lzyzsd.jsbridge.BridgeWebView
-import com.github.lzyzsd.jsbridge.BridgeWebViewClient
-
-class MyWebViewClient(webView: BridgeWebView) : BridgeWebViewClient(webView)
