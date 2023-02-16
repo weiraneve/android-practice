669c0abf8712c9d37e91daa2c9377cfccae83ca4
weiran1999 <907221539@qq.com>
2023-02-16 16:02:23 +0800

    refactor: remove excess navigation fragment and retain bottom & toolbar

--- app/src/main/java/com/thoughtworks/android/ui/navigation/NavigationActivity.kt
+++ app/src/main/java/com/thoughtworks/android/ui/navigation/NavigationActivity.kt
@@ -2,12 +2,41 @@ package com.thoughtworks.android.ui.navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.thoughtworks.android.R
 
 class NavigationActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Setup the bottom navigation view with navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
+
+        // Setup the ActionBar with navController and 3 top level destinations
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.titleScreen, R.id.leaderboard, R.id.register)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+    }
+
 }
