6c05c2c0199354d1121999d4785b584d7ed1a753
weiran1999 <907221539@qq.com>
2023-03-06 14:11:11 +0800

    feat: add SharedFlowBus

--- app/src/main/java/com/thoughtworks/android/PracticeApp.kt
+++ app/src/main/java/com/thoughtworks/android/PracticeApp.kt
@@ -1,17 +1,7 @@
 package com.thoughtworks.android
 
 import android.app.Application
-import com.thoughtworks.android.common.bus.FlowBusInitializer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PracticeApp : Application() {
-
-//    private val flowBusInitializer = FlowBusInitializer
-
-    // 避免内存泄露
-//    override fun onCreate() {
-//        super.onCreate()
-//        flowBusInitializer.init(this)
-//    }
-}
+class PracticeApp : Application() {}
--- app/src/main/java/com/thoughtworks/android/common/bus/BusDemo.kt
+++ app/src/main/java/com/thoughtworks/android/common/bus/BusDemo.kt
@@ -7,24 +7,62 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 /**
+ * LifecycleOwner usecase demo
  * need LifecycleOwner
  */
+class FlowBusDemo {
+    //private fun demo() {
+    //    observeEvent {
+    //        Log.d(
+    //            "TweetsActivity FlowBus.register3",
+    //            "FlowBus.register: ${it.get<String>("key1")}"
+    //        )
+    //    }
+    //    lifecycleScope.launch(Dispatchers.IO) {
+    //        delay(1000)
+    //        postValue(EventMessage(100))
+    //        postValue(EventMessage(101), 1000)
+    //        postValue(EventMessage(102, "bbb"), dispatcher = Dispatchers.IO)
+    //        val event3 = EventMessage(103, "ccc")
+    //        event3.put("key1", 123)
+    //        event3.put("key2", "abc")
+    //        postValue(event3, 2000, dispatcher = Dispatchers.Main)
+    //    }
+    //}
+}
 
-//private fun demo() {
-//    observeEvent {
-//        Log.d(
-//            "TweetsActivity FlowBus.register3",
-//            "FlowBus.register: ${it.get<String>("key1")}"
-//        )
-//    }
-//    lifecycleScope.launch(Dispatchers.IO) {
-//        delay(1000)
-//        postValue(EventMessage(100))
-//        postValue(EventMessage(101), 1000)
-//        postValue(EventMessage(102, "bbb"), dispatcher = Dispatchers.IO)
-//        val event3 = EventMessage(103, "ccc")
-//        event3.put("key1", 123)
-//        event3.put("key2", "abc")
-//        postValue(event3, 2000, dispatcher = Dispatchers.Main)
-//    }
-//}
+/**
+ * need application init
+ */
+class Application {
+
+    //    private val flowBusInitializer = FlowBusInitializer
+
+    // 避免内存泄露
+    //    override fun onCreate() {
+    //        super.onCreate()
+    //        flowBusInitializer.init(this)
+    //    }
+
+}
+
+/**
+ * SharedFlowBus usecase demo
+ */
+class SharedFlowBusDemo {
+    // 发送消息
+    //SharedFlowBus.with(objectKey: Class<T>).tryEmit(value: T)
+
+    // 发送粘性消息
+    //SharedFlowBus.withSticky(objectKey: Class<T>).tryEmit(value: T)
+
+    // 订阅消息
+    //SharedFlowBus.on(objectKey: Class<T>).observe(owner){ it ->
+    //    println(it)
+    //}
+
+    // 订阅粘性消息
+    //SharedFlowBus.onSticky(objectKey: Class<T>).observe(owner){ it ->
+    //    println(it)
+    //}
+}
