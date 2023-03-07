c78c8837d96783a6cee8c7c37f623c1a461a1ace
weiran1999 <907221539@qq.com>
2023-03-07 15:02:28 +0800

    feat: remove space

--- app/src/main/java/com/thoughtworks/android/ui/navigation/listscreen/ListPage.kt
+++ app/src/main/java/com/thoughtworks/android/ui/navigation/listscreen/ListPage.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 
 class ListPage : Fragment() {
@@ -23,8 +26,9 @@ class ListPage : Fragment() {
             setContent {
                 Text("HomeFragment")
                 LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    (0..100).forEach {
-                        item {
+                    (0..10).forEach {
+                        item(it) {
+                            Spacer(modifier = Modifier.height(100.dp))
                             Text(text = it.toString())
                         }
                     }
