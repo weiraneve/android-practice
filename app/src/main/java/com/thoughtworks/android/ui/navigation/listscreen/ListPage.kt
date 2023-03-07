79f1cdc003eca7f6c680e3da9844001a22ca6155
weiran1999 <907221539@qq.com>
2023-03-07 11:53:36 +0800

    feat: add num lazyColumn list page

--- app/src/main/java/com/thoughtworks/android/ui/navigation/listscreen/ListPage.kt
+++ app/src/main/java/com/thoughtworks/android/ui/navigation/listscreen/ListPage.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
-import com.thoughtworks.android.R
 
 class ListPage : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                Text("HomeFragment")
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    (0..100).forEach {
+                        item {
+                            Text(text = it.toString())
+                        }
+                    }
+                }
+            }
+        }
     }
 }
