64b5a2c280e08a0fd1b2830180aaeab9287b6fa8
weiran1999 <907221539@qq.com>
2023-03-09 09:22:44 +0800

    refactor: extract BaseActivity

---
+++ app/src/main/java/com/thoughtworks/android/common/BaseActivity.kt
@@ -0,0 +1,46 @@
+package com.thoughtworks.android.common
+
+import android.content.Intent
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
+import com.thoughtworks.android.R
+
+open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var linearLayout: LinearLayout
+
+    private val layoutParams = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+
+    fun setContentView(): LinearLayout {
+        val scrollView = ScrollView(this)
+        linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        scrollView.addView(linearLayout)
+        addContentView(scrollView, layoutParams)
+        return linearLayout
+    }
+
+    fun <T : AppCompatActivity> generateButtonAndUI(entryInt: Int, java: Class<T>) {
+        addButton(getString(entryInt)) {
+            startActivity(Intent(this, java))
+        }
+    }
+
+    private fun addButton(name: String, onClickListener: View.OnClickListener? = null) {
+        layoutParams.setMargins(0, resources.getDimensionPixelSize(R.dimen.dimen_24), 0, 0)
+        val button = Button(this)
+        button.layoutParams = layoutParams
+        button.text = name
+        button.isAllCaps = false
+        onClickListener?.let {
+            button.setOnClickListener(it)
+        }
+        linearLayout.addView(button)
+    }
+}
--- app/src/main/java/com/thoughtworks/android/ui/navigation/NavigationActivity.kt
+++ app/src/main/java/com/thoughtworks/android/ui/navigation/NavigationActivity.kt
@@ -1,51 +1,19 @@
 package com.thoughtworks.android.ui.navigation
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.thoughtworks.android.PracticeApp
 import com.thoughtworks.android.R
+import com.thoughtworks.android.common.BaseActivity
 import com.thoughtworks.android.ui.navigation.composenav.ComposeNavActivity
 import com.thoughtworks.android.ui.navigation.fragmentnav.FragmentNavActivity
 
-class NavigationActivity : AppCompatActivity() {
-
-    private val buttonContainer: LinearLayout by lazy { findViewById(R.id.nav_button_container) }
+class NavigationActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         PracticeApp.addActivity(this)
         super.onCreate(savedInstanceState)
-        startFragmentNavigationUi()
-    }
-
-    private fun startFragmentNavigationUi() {
-        setContentView(R.layout.activity_navigation)
+        setContentView()
         generateButtonAndUI(R.string.fragment_navigation, FragmentNavActivity::class.java)
         generateButtonAndUI(R.string.compose_navigation, ComposeNavActivity::class.java)
-    }
-
-    private fun <T : AppCompatActivity> generateButtonAndUI(layoutInt: Int, java: Class<T>) {
-        addButton(getString(layoutInt)) {
-            startActivity(Intent(this, java))
-        }
-    }
-
-    private fun addButton(name: String, onClickListener: View.OnClickListener? = null) {
-        val layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT
-        )
-        layoutParams.setMargins(0, resources.getDimensionPixelSize(R.dimen.dimen_24), 0, 0)
-        val button = Button(this)
-        button.layoutParams = layoutParams
-        button.text = name
-        button.isAllCaps = false
-        onClickListener?.let {
-            button.setOnClickListener(it)
-        }
-        buttonContainer.addView(button)
     }
 }
